dd7fac034301df61d633cf4f47ee01229622e1cc
jacksonkasi1 <jacksonkasipeacock@gmail.com>
2024-05-12 19:55:02 +0530

    chore: Update graphql-yoga configuration for Next.js

--- readme.md
+++ readme.md
@@ -4,3 +4,6 @@
 - https://viblo.asia/p/building-a-graphql-server-on-cloudflare-workers-with-apollo-server-v4-2024-edition-y37Ldv3y4ov
 - https://gebna.gg/blog/how-to-deploy-apollo-graphql-api-on-vercel-serverless-functions
 - https://the-guild.dev/graphql/yoga-server/docs/integrations/integration-with-nextjs
+
+
+- https://github.com/dotansimha/graphql-yoga/issues/2247
--- vercel-graphql-yoga/app/api/graphql/route.ts
+++ vercel-graphql-yoga/app/api/graphql/route.ts
@@ -1,6 +1,10 @@
 // Next.js Custom Route Handler: https://nextjs.org/docs/app/building-your-application/routing/router-handlers
-import { createSchema, createYoga } from 'graphql-yoga'
- 
+import { createSchema, createYoga } from "graphql-yoga";
+
+export const config = {
+  runtime: "edge",
+};
+
 const { handleRequest } = createYoga({
   schema: createSchema({
     typeDefs: /* GraphQL */ `
@@ -10,16 +14,21 @@ const { handleRequest } = createYoga({
     `,
     resolvers: {
       Query: {
-        greetings: () => 'This is the `greetings` field of the root `Query` type'
-      }
-    }
+        greetings: () =>
+          "This is the `greetings` field of the root `Query` type",
+      },
+    },
   }),
- 
+
   // While using Next.js file convention for routing, we need to configure Yoga to use the correct endpoint
-  graphqlEndpoint: '/api/graphql',
- 
+  graphqlEndpoint: "/api/graphql",
+
   // Yoga needs to know how to create a valid Next response
-  fetchAPI: { Response }
-})
- 
-export { handleRequest as GET, handleRequest as POST, handleRequest as OPTIONS }
+  fetchAPI: { Response },
+});
+
+export {
+  handleRequest as GET,
+  handleRequest as POST,
+  handleRequest as OPTIONS,
+};
